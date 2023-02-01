6f8d31085281a7b4c837347e499bf49d861c961d
GiannaHuang1124 <stancy1124@gmail.com>
2023-02-01 15:23:09 +0800

    no message

---
+++ Young-Artists_Advertisement/Young-Artists/Models/PartialClasses.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Young_Artists.Models
+{
+    [ModelMetadataTypeAttribute(typeof(AdvertisementMetadata))]
+    public partial class Advertisement
+    {
+    }
+
+    [ModelMetadataTypeAttribute(typeof(CompanyMetadata))]
+    public partial class Company
+    {
+
+    }
+    [ModelMetadataTypeAttribute(typeof(ContactPersonMetadata))]
+    public partial class ContactPerson
+    {
+
+    }
+}
